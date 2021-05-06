062090510dd24185267a5da8633043719d753985
Suhyun Bae <roy7579@gmail.com>
2021-05-07 02:33:14 +0900

    Update Object.scala

--- Object.scala
+++ Object.scala
@@ -1,13 +1,13 @@
 // Comparison Operator
 
 // Java에서 equals를 사용했지만, Scala는 모든 것을 ==, !=로 비교한다. 
-1 == 1
+println(1 == 1)
 -> true: Boolean
 
-'a' == 'a'
+println('a' == 'a')
 -> true: Boolean
 
-1 == 'a'
+println(1 == 'a')
 -> false: Boolean
 
 // String Comparison
@@ -15,10 +15,10 @@ val s1 = "Scala"
 val s2 = "Scala"
 val s3 = "SCALA"
 
-s1 == s2
+println(s1 == s2)
 -> true: Boolean
 
-s2 == s3
+println(s2 == s3)
 -> false: Boolean
 
 // Object Comparison
@@ -27,8 +27,8 @@ val p1 = Person("Roy", 31)
 val p2 = Person("Roy", 31)
 val p3 = Person("Clair", 27)
 
-p1 == p2
+println(p1 == p2)
 -> true: Boolean
 
-p1 == p3
+println(p1 == p3)
 -> false: Boolean
